974ca7a9728c4590498e5cf263649f66fdbcd288
Unit Test Hiweb <139615721+UnitTestHiweb@users.noreply.github.com>
2024-01-16 08:20:59 +0330

    Add files via upload

---
+++ RequestSupport/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RequestSupport")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("hiweb")]
+[assembly: AssemblyProduct("RequestSupport")]
+[assembly: AssemblyCopyright("Copyright © hiweb 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("282fec44-774f-4df2-96af-5a2911b0e969")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
